2586704e17d8f1eacefccc4c88197224d18ca135
Minuk Park <parkminuk0203@gmail.com>
2022-12-02 13:07:32 +0900

    Report

---
+++ Chapter5/src/report/Login.java
@@ -0,0 +1,14 @@
+package report;
+
+public class Login {
+	String id;
+	
+	void check () {
+		if(id.equals("박민욱")) {
+			System.out.println("아이디가 일치합니다.");
+		}
+		else {
+			System.out.println("아이디가 틀렸습니다.");
+		}
+	}
+}
---
+++ Chapter5/src/report/LoginTest.java
@@ -0,0 +1,15 @@
+package report;
+import java.util.*;
+public class LoginTest {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		Login obj = new Login();
+		
+		System.out.print("아이디 입력 : ");
+		obj.id = sc.next();
+		obj.check();
+		
+	}
+
+}
