1e40c37035a00623fc9eb1f1acd616dbe1d8e7c1
Minuk Park <parkminuk0203@gmail.com>
2022-12-02 13:38:13 +0900

    피자 토핑

---
+++ Chapter4/src/UniverSity/PizzaTopping1.java
@@ -0,0 +1,15 @@
+package UniverSity;
+
+import java.util.*;
+
+public class PizzaTopping1 {
+
+	public static void main(String[] args) {
+		String[] toppings = { "Pepperoni", "Mushrooms", "Onions", "Sausage", "Bacon" };
+		
+		for(int i=0; i<toppings.length; i++) {
+			System.out.print(toppings[i] + " ");
+		}
+	} // end of main
+
+} // end of class
---
+++ Chapter4/src/UniverSity/PizzaTopping2.java
@@ -0,0 +1,28 @@
+package UniverSity;
+
+import java.util.*;
+
+public class PizzaTopping2 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String[] arr = new String[5];
+//		arr[3] = sc.next();
+//		arr[4] = sc.next(); // next는 스페이스로 구분되어서 3번에 스페이스 입력하면 4번에 넘어가짐
+//		arr[4] = sc.nextLine(); // 띄어쓰기도 허용 엔터 칠때 까지 입력 받음
+		
+		// 토핑 입력 받아 추가
+		for(int i=0; i<arr.length; i++) {
+			System.out.println((i+1)+"번 토핑 :");
+			arr[i] = sc.next();
+		}
+		
+		// 출력
+		for(int i=0; i<arr.length; i++) {
+			System.out.print(arr[i] + " ");
+		}
+		
+		
+	} // end of main
+
+} // end of class
