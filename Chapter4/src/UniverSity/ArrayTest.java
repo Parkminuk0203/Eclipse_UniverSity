60741100a5dfbffbb0127c354d4e4faaade5cc66
Minuk Park <parkminuk0203@gmail.com>
2022-12-06 15:10:43 +0900

    복습

--- Chapter4/src/UniverSity/ArrayTest.java
+++ Chapter4/src/UniverSity/ArrayTest.java
@@ -1,27 +1,30 @@
 package UniverSity;
+
 import java.util.*;
-public class ArrayTest2 {
+
+public class ArrayTest {
 
 	public static void main(String[] args) {
 //		사용자 5명의 성적을 입력받아 평균을 구하는 프로그램
-		int[] arr = new int[5];
 		Scanner sc = new Scanner(System.in);
+		System.out.print("학생 수 입력 : ");
+		int num = sc.nextInt();
+		int[] arr = new int[num];
 		int sum=0, avg=0;
-		
 		// 성적 입력
-		for(int i=0; i<arr.length; i++) {
-			System.out.print("성적을 입력하시오:");
-			arr[i] = sc.nextInt();  
+		for (int i = 0; i < arr.length; i++) {
+			System.out.print(i+1+"번 성적을 입력하시오:");
+			arr[i] = sc.nextInt();
 		}
-		
+
 		// 평균 성적 구하기
-		for(int i=0; i<arr.length; i++) {
+		for (int i = 0; i < arr.length; i++) {
 			sum += arr[i];
 		}
-		
+
 		avg = sum / arr.length;
 		System.out.println("평균 성적은 " + avg + "입니다.");
-		
+
 	} // end of main
 
 } // end of class
--- Chapter4/src/UniverSity/Dice.java
+++ Chapter4/src/UniverSity/Dice.java
@@ -3,24 +3,23 @@ import java.util.*;
 public class Dice {
 
 	public static void main(String[] args) {
-		int[] dice = new int[100]; // 100번 저장 공간
-		int[] freq = new int[7];  // 빈도 배열
 		Random rnd = new Random();
+		int[] dice = new int[100];
+		int[] freq = new int[7];
 		
-		// 난수생성 100개
+		// 난수 생성
 		for(int i=0; i<dice.length; i++) {
-			dice[i] = rnd.nextInt(6)+1; // 1~6 난수 생성
+			dice[i] = rnd.nextInt(6)+1; // 1~6까지 난수 100개 생성 
+		}
+		// 빈도 배열을 사용하여 숫자가 나온 횟수 적립
+		for(int i=0; i<dice.length; i++) {
 			freq[dice[i]]++;
-//			dice[i] = 1 -> freq[1]++
-//			dice[i] = 2 -> freq[2]++
-//			dice[i] = 3 -> freq[3]++
-//			dice[i] = 4 -> freq[4]++
-//			dice[i] = 5 -> freq[5]++
-//			dice[i] = 6 -> freq[6]++
 		}
+		// 1~6까지 나온 횟수 출력
 		for(int i=1; i<freq.length; i++) {
-			System.out.println(i + ":" + freq[i] + "개");
+			System.out.printf("%d : %d\n",i,freq[i]);
 		}
+		
 		
 		
 	} // end of main
